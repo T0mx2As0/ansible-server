8ef05036785c7afac33188dfffad223f4ba66b90
tommaso trivelli <tommasotrivelli@MacBook-Pro-di-tommaso.local>
2025-06-03 12:33:20 +0200

    update port security group ansible

--- modules/ansible/main.tf
+++ modules/ansible/main.tf
@@ -17,25 +17,25 @@ resource "aws_security_group" "ansible_SG" {
 resource "aws_vpc_security_group_ingress_rule" "ansible_SG_ingress1" {
     security_group_id = aws_security_group.ansible_SG.id
     referenced_security_group_id = data.aws_security_groups.Bastian_Host_SG.id
-    ip_protocol = 22
+    ip_protocol = "tcp"
     to_port = 22
-    from_port = "tcp"
+    from_port = 22
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ansible_SG_ingress2" {
     security_group_id = aws_security_group.ansible_SG.id
     cidr_ipv4 = join("/",[trimspace(data.http.my_ip.response_body),"32"] )
-    ip_protocol = 22
+    ip_protocol = "tcp"
     to_port = 22
-    from_port = "tcp"
+    from_port = 22
 }
 
 resource "aws_vpc_security_group_egress_rule" "ansible_SG_egress1" {
     security_group_id = aws_security_group.ansible_SG.id
     cidr_ipv4 = data.aws_vpc.vpc.cidr_block
-    ip_protocol = 22
+    ip_protocol = "tcp"
     to_port = 22
-    from_port = "tcp"
+    from_port = 22
 }
 
 resource "aws_instance" "ansibles_server" {
